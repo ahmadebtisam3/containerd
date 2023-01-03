1c8a82f35f4d36f96ddfd7cfe2fad3cd5154157f
M. Rehan <mrehanlm93@gmail.com>
2023-01-03 12:16:09 +0500

    fixing containerd bug

--- middleware/validate.go
+++ middleware/validate.go
@@ -13,7 +13,6 @@ func ValidateSourcePath(path string) error {
 	logrus.Errorf("ix-mountc befor calling path %s", path)
 	validationErr, _ := Call("chart.release.validate_host_source_path", path)
 	logrus.Errorf("ix-mounts get results *************************")
-	logrus.Errorf("ix-mounts validation error normal error : %s ", validationErr.(string))
 	if validationErr != nil {
 		logrus.Errorf("ix-mounte inside validation error ")
 		return errors.Errorf(validationErr.(string))
